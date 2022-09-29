b3e1a77b942842f7b445897ef49ec0f99b161a75
sourClout <112970132+sourClout@users.noreply.github.com>
2022-09-29 12:58:35 -0400

    program 1 complete

--- Day01HelloWorld/Day01HelloWorld/Program.cs
+++ Day01HelloWorld/Day01HelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day01HelloWorld
 {
@@ -10,6 +6,55 @@ namespace Day01HelloWorld
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("what is your name?");
+                string name = Console.ReadLine();
+                Console.Write("What is your age?");
+                string ageStr = Console.ReadLine();
+                if (!int.TryParse(ageStr, out int age))
+                {
+                    Console.WriteLine("Invalid numerical input");
+                    return;
+                }
+                if (name.ToLower() == "santa")
+                {
+                    Console.WriteLine("SANTA! Can't believe it's really you!");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello {name}! You are {age} y/o.");
+                    Console.WriteLine("Hello {0}! You are {1} y/o. Nice to meet you, {0}.", name, age);
+
+
+                }
+
+            }
+            finally
+            {
+                Console.ReadKey();
+
+
+            }
+
+            /*
+            Console.Write("What is your name? ");
+           string name = Console.ReadLine();
+           Console.Write("What is your age? ");
+           try
+           {
+               string ageStr = Console.ReadLine();
+               int age = int.Parse(ageStr);
+
+               Console.WriteLine($"Hello {name}! You are {age} y/o!");
+           } catch (FormatException ex)
+           {
+               Console.WriteLine("Invalid numerical input");
+           }
+           Console.ReadKey();
+            */
         }
     }
 }
+
+

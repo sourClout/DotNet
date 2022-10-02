21a85f469c0565e4e39eb15590d54885aa697b18
sourClout <112970132+sourClout@users.noreply.github.com>
2022-10-02 12:00:38 -0400

    IO File Alex version

---
+++ Day01TextFile2ndTry/Day01TextFile2ndTry/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * TASK
+----
+
+Create Day01TextFile console project.
+
+Ask the user for their name.
+Write that name to "data.txt" file 3 times on separate lines.
+
+Then read back the contents of the file file (all lines, no matter how many) and display them back on the console.
+
+Make sure to handle the usual exceptions related to file IO.
+ */
+
+namespace Day01TextFile2ndTry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string filePath = @"..\..\data2.txt";
+
+                Console.Write("What is your name? ");
+                string name = Console.ReadLine();
+
+                try
+                {
+                    // write an array of strings
+                    string[] namesArray = { name, name, name };
+                    File.WriteAllLines(filePath, namesArray);
+                }
+                catch (SystemException ex)
+                {
+                    Console.WriteLine("Error writing to file: " + ex.Message);
+                }
+
+                // Reading from a file
+                try
+                {
+                    string[] linesArray = File.ReadAllLines(filePath);
+                    foreach (string line in linesArray)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                catch (SystemException ex) // (IOException ex)
+                {
+                    Console.WriteLine("Error writing to file: " + ex.Message);
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Press any key ti finish");
+                Console.ReadKey();
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ Day03WPFHelloWorld/Day03WPFHelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day03WPFHelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSayHelloLabel_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TbxName.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Name must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            LblGreeting.Content = $"Hello {name}, nice to meet you!";
+        }
+
+        private void BtnSayHelloPopup_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TbxName.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Name must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            MessageBox.Show($"Hello {name}, nice to meet you!", "Greeting", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
